7e61aa0cfa3da83559191b86293a3d7909bd48be
Vinícius Medeiros <viniciussmmedeiros@gmail.com>
2022-08-15 18:12:42 -0300

    initial commit

---
+++ api/src/main/java/dados/banco/fundamento/shoppe_fbd/ShoppeFbdApplication.java
@@ -0,0 +1,13 @@
+package dados.banco.fundamento.shoppe_fbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppeFbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppeFbdApplication.class, args);
+	}
+
+}
